a923a0264b11fb15a885c58b23cfaa67593a3083
alejandroclavera <alejandroclavera@gmail.com>
2022-10-15 20:07:04 +0200

    include react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
 import AdminPage from './components/pages/AdminPage';
 import LoginPage from './components/pages/LoginPage';
-
+import { Routes, Route } from "react-router-dom";
 
 
 function App() {
   return (
     <div className="App">
-      <LoginPage />
+      <Routes>
+        <Route path="/admin" element={<LoginPage />} />
+        <Route path="/admin/panel" element={<AdminPage />} />
+      </Routes>
+      
       {/* <AdminPage /> */}
     </div>
   );
--- src/components/pages/AdminPage.jsx
+++ src/components/pages/AdminPage.jsx
@@ -2,8 +2,6 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import ListGroup from 'react-bootstrap/ListGroup';
-import Table from 'react-bootstrap/Table';
 
 import './AdminPage.css'
 
