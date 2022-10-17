3437b0fcda6e1e881d1a15c0431f9d4dabd5ffff
Jordi Lazo <riosodr@mapfre.net>
2022-10-17 21:22:26 +0200

    organisation info final version

--- src/components/organisation/OrganisationInfo.jsx
+++ src/components/organisation/OrganisationInfo.jsx
@@ -1,61 +1,43 @@
-import Button from 'react-bootstrap/Button';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
 import Row from 'react-bootstrap/Row';
 
+import './OrganisationInfo.css'
+
 
 export default function LoginForm ({handleSubmit}) {
     return (
+        <div className="container-organisationInfo">
+            <h3 className='organisationInfo-title'>Organisation details</h3>
         <Form>
         <Row className="mb-3">
           <Form.Group as={Col} controlId="formGridEmail">
-            <Form.Label>Email</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Label>Index</Form.Label>
+            <Form.Control placeholder="Index" disabled />
           </Form.Group>
   
           <Form.Group as={Col} controlId="formGridPassword">
-            <Form.Label>Password</Form.Label>
-            <Form.Control type="password" placeholder="Password" />
+            <Form.Label>Organisation name</Form.Label>
+            <Form.Control placeholder="Organisation name" disabled />
           </Form.Group>
         </Row>
-  
-        <Form.Group className="mb-3" controlId="formGridAddress1">
-          <Form.Label>Address</Form.Label>
-          <Form.Control placeholder="1234 Main St" />
-        </Form.Group>
-  
-        <Form.Group className="mb-3" controlId="formGridAddress2">
-          <Form.Label>Address 2</Form.Label>
-          <Form.Control placeholder="Apartment, studio, or floor" />
-        </Form.Group>
-  
+
         <Row className="mb-3">
-          <Form.Group as={Col} controlId="formGridCity">
-            <Form.Label>City</Form.Label>
-            <Form.Control />
-          </Form.Group>
-  
-          <Form.Group as={Col} controlId="formGridState">
-            <Form.Label>State</Form.Label>
-            <Form.Select defaultValue="Choose...">
-              <option>Choose...</option>
-              <option>...</option>
-            </Form.Select>
-          </Form.Group>
-  
-          <Form.Group as={Col} controlId="formGridZip">
-            <Form.Label>Zip</Form.Label>
-            <Form.Control />
-          </Form.Group>
-        </Row>
-  
-        <Form.Group className="mb-3" id="formGridCheckbox">
-          <Form.Check type="checkbox" label="Check me out" />
+        <Form.Group as={Col} controlId="formGridAddress1">
+          <Form.Label>ID</Form.Label>
+          <Form.Control placeholder="ID" disabled/>
         </Form.Group>
   
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
+        <Form.Group as={Col} controlId="formGridAddress2">
+          <Form.Label>Admin</Form.Label>
+          <Form.Control placeholder="Admin" disabled />
+        </Form.Group>
+        </Row>
+        <Form.Group className="mb-3" controlId="formGridAddress1">
+        <Form.Label>Comment</Form.Label>
+        <Form.Control as="textarea" placeholder="Comment" disabled />
+      </Form.Group>
       </Form>
+      </div>
     );
 }
