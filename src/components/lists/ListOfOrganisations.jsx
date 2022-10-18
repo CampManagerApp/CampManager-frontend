c248d719a38ac8e82cbcd6f9e41d1cdf484815c3
Jordi Lazo <riosodr@mapfre.net>
2022-10-18 19:54:30 +0200

    list of organisations added

---
+++ src/components/lists/ListOfOrganisations.jsx
@@ -0,0 +1,20 @@
+import ListGroup from 'react-bootstrap/ListGroup';
+import './ListOfOrganisations.css'
+
+export default function StyleActionsExample() {
+  return (
+    <div className='test-page-wrapper'>
+    <ListGroup>
+      <ListGroup.Item action variant="secondary">
+        Organiation 1
+      </ListGroup.Item>
+      <ListGroup.Item action variant="success">
+        Organiation 1
+      </ListGroup.Item>
+      <ListGroup.Item action variant="danger">
+        Organiation 1
+      </ListGroup.Item>
+    </ListGroup>
+    </div>
+  );
+}
