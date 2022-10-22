6e8cf4720ae611e282e42de5ed3b1fd54be8c054
Jordi Lazo <riosodr@mapfre.net>
2022-10-22 21:58:58 +0200

    modified organisation list and added modal for the table list

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import SuperAdminPage from './pages/superadmin/SuperAdminPage';
 import LoginPage from './pages/authentication/LoginPage';
 import { Routes, Route } from "react-router-dom";
 import RegisterPage from './pages/authentication/RegisterPage';
-import OrganisationInfoPage from './pages/organisation/OrganisationInfoPage';
+
 import ApplicationNavbar from './components/common/ApplicationNavbar';
 import CreateOrganisationPage from './pages/organisation/CreatreOrganisationPage';
 import ListOfOrganisationsPage from './pages/users/ListOfOrganisationsPage';
@@ -19,7 +19,6 @@ function App() {
         <Route path="/admin" element={<LoginPage />} />
         <Route path="/admin/panel" element={<SuperAdminPage />} />
         <Route path="/register" element={<RegisterPage />} />
-        <Route path="/organisationinfo" element={<OrganisationInfoPage />} />
         <Route path="/admin/organisation/create" element={<CreateOrganisationPage />} />
         <Route path="/admin/organisation/update" element={<UpdateOrganisationPage />} >
           <Route path=":organisationId" element={<UpdateOrganisationPage />} />
