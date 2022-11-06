63d0054949df32e75cbb63b0163b650450cda1d3
alejandroclavera <alejandroclavera@gmail.com>
2022-11-06 11:06:07 +0100

    add adminServices file

--- src/services/admin/adminServices.js
+++ src/services/admin/adminServices.js
@@ -3,32 +3,5 @@ import { loggedRequest } from "../../config";
 
 
 export function getOrganisationMembers() {
-    return [
-        {
-            id:uuidv4(),
-            name: 'USER1',
-            role:'admin'
-        },
-        {
-            id:uuidv4(),
-            name: 'USER2',
-            role:'admin'
-        },
-        {
-            id:uuidv4(),
-            name: 'USER3',
-            role:'cousellor'
-        },
-        {
-            id:uuidv4(),
-            name: 'USER3',
-            role:'cousellor'
-        },
-
-        {
-            id:uuidv4(),
-            name: 'USER4',
-            role:'admin'
-        }
-    ]
+    //return await 
 }
