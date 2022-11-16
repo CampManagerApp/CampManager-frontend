b6d2ed1ecdba4214d3b14877b5c515742794b2e1
Carlos <carlos.isaac@intarex.com>
2022-11-16 19:13:38 +0100

    + cambios de profile y empezar pagina camp

--- src/components/common/ProfilePage.jsx
+++ src/components/common/ProfilePage.jsx
@@ -5,23 +5,24 @@ import './ProfilePage.css'
  profileName
  profileNick
  backgroundImg
- idVisible
+ idVisible : visible | hidden | collapse | inherit
+ includeProfileImage : 'hidden'
 
 */
 export default function ProfilePage(props){
     return(
         <div>
-            <div className="top-photo col-md-12">
+            <div className="top-photo col-md-12 ">
                 <img src={props.backgroundImg}/>
             </div>
             <div className='profile'>
-                <div className='profile-img'>
+                <div className='profile-img' style={{display:props.includeProfileImage}}>
                     <img src={props.profileImg} 
                     alt="Circle Image" className="img-raised rounded-circle img-fluid"/>{' '} 
                 </div>
                 <div className='profile-inf'>
                     <p className='profile-text'>{props.profileName}</p> 
-                    <p className='profile-text' style={{visibility:props.idVisible}}>@{props.profileNick}</p>
+                    <p className='id-text' style={{visibility:props.idVisible}}>@{props.profileNick}</p>
                 </div>
 
             </div>
