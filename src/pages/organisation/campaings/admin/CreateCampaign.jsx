c21f8fdb11ee57900c78f7901294c72e59fd4923
alejandroclavera <alejandroclavera@gmail.com>
2022-12-08 13:01:55 +0100

    include prime react

--- src/pages/organisation/campaings/admin/CreateCampaign.jsx
+++ src/pages/organisation/campaings/admin/CreateCampaign.jsx
@@ -2,20 +2,23 @@ import BannerImage from "../../../../components/common/BannerImage"
 import TitlePage from "../../../../components/common/TitlePage";
 import * as image from "../../../../design/images";
 import Form from 'react-bootstrap/Form';
-import DatePicker from 'react-datepicker';
 import React, { useEffect, useState } from "react";
+
+import { Calendar } from 'primereact/calendar';
+
 
 
 export default function CreateCampaign() {
     const [startDate, setStartDate] = useState(new Date());
+    
     return (
         <React.Fragment>
-            <BannerImage bannerImage={image.backgroundOrg}/>
+            <BannerImage bannerImage={image.backgroundOrg} />
             <TitlePage>Create campaigns</TitlePage>
-            <><Form.Label>Campaign name:</Form.Label><Form.Control type="text" aria-describedby="passwordHelpBlock"/></>
+            <><Form.Label>Campaign name:</Form.Label><Form.Control type="text" aria-describedby="passwordHelpBlock" /></>
             <><Form.Label>Duration:</Form.Label></>
-            <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} />
-            
+            {/* <DatePicker selected={startDate} onChange={(date) => setStartDate(date)} /> */}
+            <Calendar value={startDate} onChange={(e) => setStartDate(e.value)}></Calendar>
         </React.Fragment>
     )
 }
