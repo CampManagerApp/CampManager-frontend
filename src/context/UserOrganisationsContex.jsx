3a281781ef12f49b167171006e8d8aa48d665060
alejandroclavera <alejandroclavera@gmail.com>
2022-12-08 12:45:24 +0100

    request user organisation in the user organisations page

--- src/context/UserOrganisationsContex.jsx
+++ src/context/UserOrganisationsContex.jsx
@@ -1,15 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { createContext } from "react";
 import { Organisations } from "../data/organisations";
+import { get_usr_organisations } from "../services/users/Users";
 
 export const UserOrganisationsContex = createContext()
 
 export default function UserOrganisationsProvider(props) {
-    const [orgUserList, setOrgUserList] = useState([Organisations[2], Organisations[3]])
+    const [orgUserList, setOrgUserList] = useState([])
+
+    useEffect(() => {
+        loadUserOrganisation()
+    }, [])
     
     const orgList = orgUserList.map((org) => {
         return org
     })
+
+    async function loadUserOrganisation() {
+        try {
+            const organisations = await get_usr_organisations('admin')
+            setOrgUserList(organisations)
+        } catch (error) {
+            
+        }
+    }
+
     
     function addOrg(org) {
         setOrgUserList([...orgUserList, org])
